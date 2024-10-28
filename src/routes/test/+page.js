3b3a8820f46c4ea211463002cbde4dffbda98a28
eltigerchino <chewteeming01@gmail.com>
2024-10-28 10:45:14 +0800

    Initial commit

---
+++ src/routes/test/+page.js
@@ -0,0 +1,10 @@
+export const load = async ({ fetch }) => {
+	const res = await fetch('https://httpbin.org/json');
+	console.log(res);
+
+	const data = await res.json();
+
+	return { data };
+};
+
+export const prerender = true;
